9c00d4d820f490feb73f48f58f469bef52ab6090
Andrew Bruce <me@andrewbruce.net>
2019-05-07 15:29:27 +0100

    Add LegacyOrderSubmitted and associated types
    This type is used by Terry, the order service.
    
    [#165441236]
    
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

---
+++ src/main/java/com/boclips/events/types/LegacyOrderSubmitted.java
@@ -0,0 +1,153 @@
+package com.boclips.events.types;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import lombok.*;
+import lombok.experimental.NonFinal;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class LegacyOrderExtraFields {
+    private Boolean agreeTerms;
+
+    @JsonProperty("isbn_or_product_number")
+    private String isbnOrProductNumber;
+}
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class LegacyOrderNextStatus {
+    @NonNull
+    private List<String> roles;
+
+    @NonNull
+    @JsonProperty("next_states")
+    private List<String> nextStates;
+}
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+class LegacyOrder {
+    @NonNull
+    private String id;
+
+    @NonNull
+    @JsonProperty("uuid")
+    private String uuid;
+
+    @NonNull
+    private String status;
+
+    @NonNull
+    private String vendor;
+
+    @NonNull
+    private String creator;
+
+    @NonNull
+    @JsonProperty("date_updated")
+    private Date dateUpdated;
+
+    @NonNull
+    @JsonProperty("date_created")
+    private Date dateCreated;
+
+    @NonNull
+    @JsonProperty("extra_fields")
+    private LegacyOrderExtraFields extraFields;
+
+    @NonNull
+    @JsonProperty("next_status")
+    private LegacyOrderNextStatus nextStatus;
+}
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+class LegacyOrderItemLicense {
+    @NonNull
+    private String id;
+
+    @NonNull
+    @JsonProperty("uuid")
+    private String uuid;
+
+    @NonNull
+    @JsonProperty("date_updated")
+    private Date dateUpdated;
+
+    @NonNull
+    @JsonProperty("date_created")
+    private Date dateCreated;
+
+    @NonNull
+    private String code;
+
+    @NonNull
+    private String description;
+}
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+class LegacyOrderItem {
+    @NonNull
+    private String id;
+
+    @NonNull
+    @JsonProperty("uuid")
+    private String uuid;
+
+    @NonNull
+    @JsonProperty("asset_id")
+    private String assetId;
+
+    @NonNull
+    @JsonProperty("date_updated")
+    private Date dateUpdated;
+
+    @NonNull
+    @JsonProperty("date_created")
+    private Date dateCreated;
+
+    @NonNull
+    private LegacyOrderItemLicense license;
+
+    @NonNull
+    private BigDecimal price;
+
+    @NonNull
+    @JsonProperty("transcripts_required")
+    private Boolean transcriptsRequired;
+
+    @NonNull
+    private String status;
+}
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LegacyOrderSubmitted {
+    @NonNull
+    private LegacyOrder order;
+
+    @NonNull
+    private List<LegacyOrderItem> orderItems;
+}
---
+++ src/test/java/com/boclips/events/types/TestWithJsonFixture.java
@@ -0,0 +1,19 @@
+package com.boclips.events.types;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+class TestWithJsonFixture {
+    String loadExample(String filename) throws IOException {
+        try (InputStream stream = getClass().getClassLoader().getResourceAsStream(filename)) {
+            try (InputStreamReader reader = new InputStreamReader(stream)) {
+                try (BufferedReader bufferedReader = new BufferedReader(reader)) {
+                    return bufferedReader.lines().collect(Collectors.joining());
+                }
+            }
+        }
+    }
+}
