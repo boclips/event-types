3f42b88d6ccc67f43b6f1a5edd939f9b486156c7
Andrew Bruce <me@andrewbruce.net>
2019-05-09 11:40:15 +0100

    Make some legacy order classes public
    ...so they can be constructed in e.g. application tests.
    
    [#165441236]

---
+++ src/main/java/com/boclips/events/types/LegacyOrderExtraFields.java
@@ -0,0 +1,18 @@
+package com.boclips.events.types;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LegacyOrderExtraFields {
+    private Boolean agreeTerms;
+
+    @JsonProperty("isbn_or_product_number")
+    private String isbnOrProductNumber;
+}
--- src/main/java/com/boclips/events/types/LegacyOrderSubmitted.java
+++ src/main/java/com/boclips/events/types/LegacyOrderSubmitted.java
@@ -12,69 +12,6 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-class LegacyOrderExtraFields {
-    private Boolean agreeTerms;
-
-    @JsonProperty("isbn_or_product_number")
-    private String isbnOrProductNumber;
-}
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-class LegacyOrderNextStatus {
-    @NonNull
-    private List<String> roles;
-
-    @NonNull
-    @JsonProperty("next_states")
-    private List<String> nextStates;
-}
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
-class LegacyOrder {
-    @NonNull
-    private String id;
-
-    @NonNull
-    @JsonProperty("uuid")
-    private String uuid;
-
-    @NonNull
-    private String status;
-
-    @NonNull
-    private String vendor;
-
-    @NonNull
-    private String creator;
-
-    @NonNull
-    @JsonProperty("date_updated")
-    private Date dateUpdated;
-
-    @NonNull
-    @JsonProperty("date_created")
-    private Date dateCreated;
-
-    @NonNull
-    @JsonProperty("extra_fields")
-    private LegacyOrderExtraFields extraFields;
-
-    @NonNull
-    @JsonProperty("next_status")
-    private LegacyOrderNextStatus nextStatus;
-}
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 class LegacyOrderItemLicense {
     @NonNull
