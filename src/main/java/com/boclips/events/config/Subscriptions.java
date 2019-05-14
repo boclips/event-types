b3b18b3e8fc95b938f1494fd25d4b7c2ade7145b
Matt Jones <matt@boclips.com>
2019-05-14 16:47:11 +0100

    Add remaining topics for each new type
    [#165930666]

--- src/main/java/com/boclips/events/config/Subscriptions.java
+++ src/main/java/com/boclips/events/config/Subscriptions.java
@@ -15,6 +15,16 @@ public interface Subscriptions {
     String LEGACY_ORDER_SUBMITTED = TopicNames.LEGACY_ORDER_SUBMITTED + SUFFIX;
     String USER_ACTIVATED = TopicNames.USER_ACTIVATED + SUFFIX;
     String VIDEOS_SEARCHED = TopicNames.VIDEOS_SEARCH + SUFFIX;
+    String VIDEO_SEGMENT_PLAYED = TopicNames.VIDEO_SEGMENT_PLAYED + SUFFIX;
+    String COLLECTION_AGE_RANGE_CHANGED = TopicNames.COLLECTION_AGE_RANGE_CHANGED + SUFFIX;
+    String COLLECTION_BOOKMARKED = TopicNames.COLLECTION_BOOKMARKED + SUFFIX;
+    String COLLECTION_MADE_PRIVATE = TopicNames.COLLECTION_MADE_PRIVATE + SUFFIX;
+    String COLLECTION_MADE_PUBLIC = TopicNames.COLLECTION_MADE_PUBLIC + SUFFIX;
+    String COLLECTION_RENAMED = TopicNames.COLLECTION_RENAMED + SUFFIX;
+    String COLLECTION_SUBJECTS_CHANGED = TopicNames.COLLECTION_SUBJECTS_CHANGED + SUFFIX;
+    String COLLECTION_UNBOOKARMED = TopicNames.COLLECTION_UNBOOKARMED + SUFFIX;
+    String VIDEO_REMOVED_FROM_COLLECTION = TopicNames.VIDEO_REMOVED_FROM_COLLECTION + SUFFIX;
+    String VIDEO_ADDED_TO_COLLECTION = TopicNames.VIDEO_ADDED_TO_COLLECTION + SUFFIX;
 
     @Input(VIDEO_PLAYBACK_SYNC_REQUESTED)
     SubscribableChannel videoPlaybackSyncRequested();
@@ -36,4 +46,34 @@ public interface Subscriptions {
 
     @Input(VIDEOS_SEARCHED)
     SubscribableChannel videosSearched();
+
+    @Input(VIDEO_SEGMENT_PLAYED)
+    SubscribableChannel videoSegmentPlayed();
+
+    @Input(COLLECTION_AGE_RANGE_CHANGED)
+    SubscribableChannel collectionAgeRangeChanged();
+
+    @Input(COLLECTION_BOOKMARKED)
+    SubscribableChannel collectionBookmarked();
+
+    @Input(COLLECTION_MADE_PRIVATE)
+    SubscribableChannel collectionMadePrivate();
+
+    @Input(COLLECTION_MADE_PUBLIC)
+    SubscribableChannel collectionMadePublic();
+
+    @Input(COLLECTION_RENAMED)
+    SubscribableChannel collectionRenamed();
+
+    @Input(COLLECTION_SUBJECTS_CHANGED)
+    SubscribableChannel collectionSubjectsChanged();
+
+    @Input(COLLECTION_UNBOOKARMED)
+    SubscribableChannel collectionUnbookmarked();
+
+    @Input(VIDEO_REMOVED_FROM_COLLECTION)
+    SubscribableChannel videoRemovedFromCollection();
+
+    @Input(VIDEO_ADDED_TO_COLLECTION)
+    SubscribableChannel videoAddedToCollection();
 }
--- src/main/java/com/boclips/events/config/Topics.java
+++ src/main/java/com/boclips/events/config/Topics.java
@@ -11,6 +11,17 @@ public interface Topics {
     String VIDEO_ANALYSED = TopicNames.VIDEO_ANALYSED + SUFFIX;
     String USER_ACTIVATED = TopicNames.USER_ACTIVATED + SUFFIX;
     String VIDEOS_SEARCHED = TopicNames.VIDEOS_SEARCH + SUFFIX;
+    String VIDEO_SEGMENT_PLAYED = TopicNames.VIDEO_SEGMENT_PLAYED + SUFFIX;
+    String COLLECTION_AGE_RANGE_CHANGED = TopicNames.COLLECTION_AGE_RANGE_CHANGED + SUFFIX;
+    String COLLECTION_BOOKMARKED = TopicNames.COLLECTION_BOOKMARKED + SUFFIX;
+    String COLLECTION_MADE_PRIVATE = TopicNames.COLLECTION_MADE_PRIVATE + SUFFIX;
+    String COLLECTION_MADE_PUBLIC = TopicNames.COLLECTION_MADE_PUBLIC + SUFFIX;
+    String COLLECTION_RENAMED = TopicNames.COLLECTION_RENAMED + SUFFIX;
+    String COLLECTION_SUBJECTS_CHANGED = TopicNames.COLLECTION_SUBJECTS_CHANGED + SUFFIX;
+    String COLLECTION_UNBOOKARMED = TopicNames.COLLECTION_UNBOOKARMED + SUFFIX;
+    String VIDEO_ADDED_TO_COLLECTION = TopicNames.VIDEO_ADDED_TO_COLLECTION + SUFFIX;
+    String VIDEO_REMOVED_FROM_COLLECTION = TopicNames.VIDEO_REMOVED_FROM_COLLECTION + SUFFIX;
+
 
     @Output(VIDEO_PLAYBACK_SYNC_REQUESTED)
     MessageChannel videoPlaybackSyncRequested();
@@ -29,4 +40,34 @@ public interface Topics {
 
     @Output(VIDEOS_SEARCHED)
     MessageChannel videosSearched();
+
+    @Output(VIDEO_SEGMENT_PLAYED)
+    MessageChannel videoSegmentPlayed();
+
+    @Output(COLLECTION_AGE_RANGE_CHANGED)
+    MessageChannel collectionAgeRangeChanged();
+
+    @Output(COLLECTION_BOOKMARKED)
+    MessageChannel collectionBookmarked();
+
+    @Output(COLLECTION_MADE_PRIVATE)
+    MessageChannel collectionMadePrivate();
+
+    @Output(COLLECTION_MADE_PUBLIC)
+    MessageChannel collectionMadePublic();
+
+    @Output(COLLECTION_RENAMED)
+    MessageChannel collectionRenamed();
+
+    @Output(COLLECTION_SUBJECTS_CHANGED)
+    MessageChannel collectionSubjectsChanged();
+
+    @Output(COLLECTION_UNBOOKARMED)
+    MessageChannel collectionUnbookmarked();
+
+    @Output(VIDEO_REMOVED_FROM_COLLECTION)
+    MessageChannel videoRemovedFromCollection();
+
+    @Output(VIDEO_ADDED_TO_COLLECTION)
+    MessageChannel videoAddedToCollection();
 }
