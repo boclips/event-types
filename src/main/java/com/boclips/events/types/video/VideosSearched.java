03ffc72357310a3ffc9ed84bc29a3eeb8966e2bf
Jacek Rzeniewicz <jacek@boclips.com>
2019-05-17 10:03:12 +0100

    Make VideosSearched a subclass of UserEvent

--- src/main/java/com/boclips/events/types/video/VideosSearched.java
+++ src/main/java/com/boclips/events/types/video/VideosSearched.java
@@ -1,7 +1,6 @@
 package com.boclips.events.types.video;
 
-import com.boclips.events.types.User;
-import com.boclips.events.types.base.Event;
+import com.boclips.events.types.base.UserEvent;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -10,10 +9,7 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-public class VideosSearched extends Event {
-
-    @NonNull
-    private User user;
+public class VideosSearched extends UserEvent {
 
     @NonNull
     private String query;
