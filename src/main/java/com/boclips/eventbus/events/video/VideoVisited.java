a7b164714b51d5c6015959a27d1b60e465a29a03
Alex Glen <alex@boclips.com>
2019-08-08 17:00:17 +0100

    Add new video visited event
    [#167668560]
    
    Signed-off-by: Matt Jones <matt@boclips.com>

---
+++ src/main/java/com/boclips/eventbus/events/video/VideoVisited.java
@@ -0,0 +1,28 @@
+package com.boclips.eventbus.events.video;
+
+
+import com.boclips.eventbus.BoclipsEvent;
+import com.boclips.eventbus.events.base.UserEvent;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@BoclipsEvent("video-visited")
+public class VideoVisited extends UserEvent {
+
+    @NonNull
+    private String videoId;
+
+    private String searchQuery;
+
+    private Integer searchIndex;
+
+    private Integer pageIndex;
+
+    private Integer pageSize;
+
+}
