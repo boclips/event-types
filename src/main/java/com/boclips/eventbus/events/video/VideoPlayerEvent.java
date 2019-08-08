8119e12a818ec54843a019c65f3e769844f93f71
Jacek Rzeniewicz <jacek@boclips.com>
2019-08-08 15:04:15 +0100

    Rename consumerDevice -> playbackDevice

--- src/main/java/com/boclips/eventbus/events/video/VideoPlayerEvent.java
+++ src/main/java/com/boclips/eventbus/events/video/VideoPlayerEvent.java
@@ -20,6 +20,6 @@ public class VideoPlayerEvent extends UserEvent {
     @NonNull
     private Long videoDurationSeconds;
 
-    private String consumerDevice;
+    private String playbackDevice;
 
 }
