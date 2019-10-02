5f73c85173e274d1c35bd3501fe667f4c17dabc5
Raul Vences Paradela <raul@boclips.com>
2019-10-02 11:59:51 +0100

    Add missing NoArgsConstructor to UserCreated
    Signed-off-by: Jacek Rzeniewicz <jacek@boclips.com>

--- src/main/java/com/boclips/eventbus/events/user/UserCreated.java
+++ src/main/java/com/boclips/eventbus/events/user/UserCreated.java
@@ -3,16 +3,14 @@ package com.boclips.eventbus.events.user;
 import com.boclips.eventbus.BoclipsEvent;
 import com.boclips.eventbus.domain.user.Organisation;
 import com.boclips.eventbus.events.base.AbstractUserEvent;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @SuperBuilder
 @AllArgsConstructor
+@NoArgsConstructor
 @BoclipsEvent("user-created")
 public class UserCreated extends AbstractUserEvent {
 
