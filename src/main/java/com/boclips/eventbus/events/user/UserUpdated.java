284dabe1d3bb904cfef2e5c45a9a4b5f8f39b767
Raul Vences Paradela <raul@boclips.com>
2019-10-25 14:30:56 +0100

    Drop redundant annotations
    Signed-off-by: Jacek Rzeniewicz <jacek@boclips.com>

--- src/main/java/com/boclips/eventbus/events/user/UserUpdated.java
+++ src/main/java/com/boclips/eventbus/events/user/UserUpdated.java
@@ -2,14 +2,9 @@ package com.boclips.eventbus.events.user;
 
 import com.boclips.eventbus.BoclipsEvent;
 import com.boclips.eventbus.events.base.AbstractEventWithUser;
-import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-@Getter
-@Setter
 @SuperBuilder
-@AllArgsConstructor
-@NoArgsConstructor
 @BoclipsEvent("user-updated")
 public class UserUpdated extends AbstractEventWithUser {
 }
