4079f9f941abee973224291f1cda8cea61550f96
Jacek Rzeniewicz <jacek@boclips.com>
2019-12-02 16:29:02 +0000

    Add support for Java 8 date and time types

--- src/main/java/com/boclips/eventbus/infrastructure/ObjectMapperProvider.java
+++ src/main/java/com/boclips/eventbus/infrastructure/ObjectMapperProvider.java
@@ -1,10 +1,15 @@
 package com.boclips.eventbus.infrastructure;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class ObjectMapperProvider {
 
     public static ObjectMapper get() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
     }
 }
--- src/test/java/com/boclips/eventbus/infrastructure/ObjectMapperProviderTest.java
+++ src/test/java/com/boclips/eventbus/infrastructure/ObjectMapperProviderTest.java
@@ -1,20 +1,65 @@
 package com.boclips.eventbus.infrastructure;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ObjectMapperProviderTest {
 
+    private AnEvent event = new AnEvent();
+
+    private ObjectMapper objectMapper = ObjectMapperProvider.get();
+
     @Test
     public void enumValuesAreWrittenAsStrings() throws JsonProcessingException {
-        AnEvent event = new AnEvent();
         event.setEnumField(AnEnum.A_VALUE);
 
-        String json = ObjectMapperProvider.get().writeValueAsString(event);
+        String json = objectMapper.writeValueAsString(event);
 
         assertThat(json).contains("\"A_VALUE\"");
+    }
+
+    @Test
+    public void dateValuesAreWrittenAsArrays() throws JsonProcessingException {
+        event.setDateField(LocalDate.ofYearDay(2020, 12));
+
+        String json = objectMapper.writeValueAsString(event);
+
+        assertThat(json).contains("\"2020-01-12\"");
+    }
+
+    @Test
+    public void dateValuesCanBeRestored() throws JsonProcessingException {
+        event.setDateField(LocalDate.ofYearDay(2021, 22));
+
+        String json = objectMapper.writeValueAsString(event);
+        AnEvent restoredEvent = objectMapper.readValue(json, AnEvent.class);
+
+        assertThat(restoredEvent.dateField).isEqualTo("2021-01-22");
+    }
+
+    @Test
+    public void datetimeValuesAreWrittenStrings() throws JsonProcessingException {
+        event.setDateTimeField(ZonedDateTime.parse("2019-12-07T13:12:11Z"));
+
+        String json = objectMapper.writeValueAsString(event);
+
+        assertThat(json).contains("2019-12-07T13:12:11Z");
+    }
+
+    @Test
+    public void datetimeValuesCanBeRestored() throws JsonProcessingException {
+        event.setDateTimeField(ZonedDateTime.parse("2019-11-07T13:12:11Z"));
+
+        String json = objectMapper.writeValueAsString(event);
+        AnEvent restoredEvent = objectMapper.readValue(json, AnEvent.class);
+
+        assertThat(restoredEvent.dateTimeField).isEqualTo("2019-11-07T13:12:11Z");
     }
 
     enum AnEnum {
@@ -25,12 +70,32 @@ public class ObjectMapperProviderTest {
 
         private AnEnum enumField;
 
+        private LocalDate dateField;
+
+        private ZonedDateTime dateTimeField;
+
         public AnEnum getEnumField() {
             return enumField;
         }
 
         public void setEnumField(AnEnum enumField) {
             this.enumField = enumField;
+        }
+
+        public LocalDate getDateField() {
+            return dateField;
+        }
+
+        public void setDateField(LocalDate dateField) {
+            this.dateField = dateField;
+        }
+
+        public ZonedDateTime getDateTimeField() {
+            return dateTimeField;
+        }
+
+        public void setDateTimeField(ZonedDateTime dateTimeField) {
+            this.dateTimeField = dateTimeField;
         }
     }
 }
