c969d7c3e89ca80bb8314e67def08ae263db7181
Jacek Rzeniewicz <jacek@rzrz.pl>
2020-05-28 13:56:32 +0100

    Remove deprecated channel properties
    They had been nested in details objects.

--- src/main/java/com/boclips/eventbus/domain/contentpartner/Channel.java
+++ src/main/java/com/boclips/eventbus/domain/contentpartner/Channel.java
@@ -1,11 +1,6 @@
 package com.boclips.eventbus.domain.contentpartner;
 
-import com.boclips.eventbus.domain.AgeRange;
 import lombok.*;
-
-import java.time.Period;
-import java.util.List;
-import java.util.Locale;
 
 @Data
 @Builder
@@ -25,35 +20,4 @@ public class Channel {
     private ChannelIngestDetails ingest;
 
     private ChannelMarketingDetails marketing;
-
-    @NonNull
-    @Deprecated
-    private AgeRange ageRange;
-
-    @Deprecated
-    private String legalRestrictions;
-
-    @Deprecated
-    private String description;
-
-    @Deprecated
-    private List<String> contentTypes;
-
-    @Deprecated
-    private List<String> contentCategories;
-
-    @Deprecated
-    private Locale language;
-
-    @Deprecated
-    private String hubspotId;
-
-    @Deprecated
-    private String awards;
-
-    @Deprecated
-    private String notes;
-
-    @Deprecated
-    private Period deliveryFrequency;
 }
