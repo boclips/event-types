ea5c1857c3c27309fd972b96f60427edd2aa90f7
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2020-06-16 15:26:08 +0100

    Add new collection events
    [#173036532]

--- src/main/java/com/boclips/eventbus/events/collection/CollectionInteractionType.java
+++ src/main/java/com/boclips/eventbus/events/collection/CollectionInteractionType.java
@@ -2,5 +2,7 @@ package com.boclips.eventbus.events.collection;
 
 public enum CollectionInteractionType {
     NAVIGATE_TO_COLLECTION_DETAILS,
-    VISIT_LESSON_GUIDE
+    VISIT_LESSON_GUIDE,
+    COLLECTION_LINK_COPIED,
+    COLLECTION_SHARED_TO_GOOGLE_CLASSROOM
 }
